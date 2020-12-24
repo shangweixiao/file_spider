f6394dc486552bc981d325e23413b14816d3b505
shangwx <shangweixiao@msn.com>
2020-12-24 16:54:57 +0800

    update

--- main2.py
+++ main2.py
@@ -106,7 +106,7 @@ class SpiderDownload:
         self.threadcnt = 0
         self.DownloadedCnt = 0
 
-        self.header={'Host': 'mirrors.sohu.com',
+        self.header={'Host': 'mirrors.kernel.org',
            'Upgrade-Insecure-Requests': '1',
            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
         }
